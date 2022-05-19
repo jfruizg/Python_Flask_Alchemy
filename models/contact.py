df9872fc8f89e37fcfdeae215b35ff93a275b9c1
juanf <jfruizg@unbosque.edu.co>
2022-05-19 08:41:25 -0500

    Adds SQLAlchemy function to the database

--- models/contact.py
+++ models/contact.py
@@ -1,7 +1,7 @@
 from utils.db import db
 
 class Contact(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
+    id = db.Column(db.Integer, primary_key=True)
     fullname = db.Column(db.String(100))
     email = db.Column(db.String(100))
     phone = db.Column(db.String(100))
