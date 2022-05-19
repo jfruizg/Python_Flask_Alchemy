6d5af3ff6461840e0f00cc6be4cb83f3b3f1f61b
juanf <jfruizg@unbosque.edu.co>
2022-05-19 11:01:25 -0500

    Add routes to category model

--- app.py
+++ app.py
@@ -3,20 +3,11 @@ from routes.contact import contact
 from routes.category import task
 from flask_sqlalchemy import SQLAlchemy
 
-prod = Flask(__name__)
-local = Flask(__name__)
+app = Flask(__name__)
 
-local.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:felipe1972@localhost/api_flask'
-local.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://bab79d65de65fd:2089d769@us-cdbr-east-05.cleardb.net/heroku_f4bb183c903f077'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-prod.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://bab79d65de65fd:2089d769@us-cdbr-east-05.cleardb.net/heroku_f4bb183c903f077'
-prod.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+SQLAlchemy(app)
 
-SQLAlchemy(prod)
-SQLAlchemy(local)
-
-prod.register_blueprint(contact)
-local.register_blueprint(contact)
-
-prod.register_blueprint(task)
-local.register_blueprint(task)
+app.register_blueprint(contact)
