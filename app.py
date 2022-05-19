9793f3992fb4d6e5a6808f811d70bf25c0a6531e
juanf <jfruizg@unbosque.edu.co>
2022-05-19 09:48:42 -0500

    Add connection heroku

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://b56a86448bafb:bf464afa@us-cdbr-east-05.cleardb.net/heroku_71115a8dcdb2348'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://bab79d65de65fd:2089d769@us-cdbr-east-05.cleardb.net/heroku_f4bb183c903f077'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 SQLAlchemy(app)
