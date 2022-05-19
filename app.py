45d6231f2fe826ce66a4ab62aa7e60153fafbba2
juanf <jfruizg@unbosque.edu.co>
2022-05-19 10:56:09 -0500

    Add routes to category model

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask
 from routes.contact import contact
+from routes.category import task
 from flask_sqlalchemy import SQLAlchemy
 
 prod = Flask(__name__)
-
 local = Flask(__name__)
 
 local.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:felipe1972@localhost/api_flask'
@@ -17,3 +17,6 @@ SQLAlchemy(local)
 
 prod.register_blueprint(contact)
 local.register_blueprint(contact)
+
+prod.register_blueprint(task)
+local.register_blueprint(task)
--- models/contact.py
+++ models/contact.py
@@ -5,8 +5,23 @@ class Contact(db.Model):
     fullname = db.Column(db.String(100))
     email = db.Column(db.String(100))
     phone = db.Column(db.String(100))
+    tasks = db.relationship('task', lazy='dynamic')
 
     def __init__(self, fullname, email, phone):
         self.fullname = fullname
         self.email = email
         self.phone = phone
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50))
+    description = db.Column(db.Text())
+    contact_id = db.Column(db.Integer, db.ForeignKey('contact.id'))
+    contact = db.relationship("Contact")
+
+    def __init__(self, title, description, contact_id):
+        self.title = title
+        self.description = description
+        self.contact_id = contact_id
+
+
