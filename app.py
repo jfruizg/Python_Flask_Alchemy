2fc3b01fadb2e7d41924c964981f3ca49669d060
juanf <jfruizg@unbosque.edu.co>
2022-05-19 09:59:10 -0500

    Add local and prod

--- app.py
+++ app.py
@@ -2,11 +2,18 @@ from flask import Flask
 from routes.contact import contact
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+prod = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://bab79d65de65fd:2089d769@us-cdbr-east-05.cleardb.net/heroku_f4bb183c903f077'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+local = Flask(__name__)
 
-SQLAlchemy(app)
+local.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:felipe1972@localhost/api_flask'
+local.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-app.register_blueprint(contact)
+prod.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://bab79d65de65fd:2089d769@us-cdbr-east-05.cleardb.net/heroku_f4bb183c903f077'
+prod.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+SQLAlchemy(prod)
+SQLAlchemy(local)
+
+prod.register_blueprint(contact)
+local.register_blueprint(contact)
--- index.py
+++ index.py
@@ -1,9 +1,18 @@
-from app import app
+from app import local, prod
 from utils.db import db
 
-with app.app_context():
+with prod.app_context():
+    db.create_all()
+
+with local.app_context():
     db.create_all()
 
 if __name__ == "__main__":
-  app.run(debug=True)
+  variable = input()
+  if variable == "local":
+      local.run(debug=True)
+  else:
+      prod.run(debug=True)
+
+
 
