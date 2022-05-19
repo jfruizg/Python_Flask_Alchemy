e4a12cc78ac6e4ff43e06d3b9bb8d525b8f98462
juanf <jfruizg@unbosque.edu.co>
2022-05-19 11:15:05 -0500

    Add local and prod

--- app.py
+++ app.py
@@ -3,10 +3,16 @@ from routes.contact import contact
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+local = Flask(__name__)
+
+local.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:felipe1972@localhost/api_flask'
+local.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://bab79d65de65fd:2089d769@us-cdbr-east-05.cleardb.net/heroku_f4bb183c903f077'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 SQLAlchemy(app)
+SQLAlchemy(local)
 
 app.register_blueprint(contact)
+local.register_blueprint(contact)
